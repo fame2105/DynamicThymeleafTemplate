bb21b9ddc8921afecbf1750729f5afee155c81f5
unknown <fame.issrani@coditas.com>
2019-10-09 14:22:22 +0530

    Configured ClassLoaderTemplateResolver to be used in case there are any static templates present under resources/templates othervise StringResourceTemplateResolver

--- src/main/java/com/learn/Controller/MyController.java
+++ src/main/java/com/learn/Controller/MyController.java
@@ -3,30 +3,39 @@ package com.learn.Controller;
 import com.learn.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
+
+import javax.swing.*;
 
 @RequestMapping("/api")
 @RestController
 public class MyController {
 
     private final SpringTemplateEngine templateEngine;
-    private final SpringTemplateEngine templateService;
     private static final Logger log = LoggerFactory.getLogger(MyController.class);
 
-    public MyController(SpringTemplateEngine templateEngine, SpringTemplateEngine templateService) {
+    public MyController(SpringTemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
-        this.templateService = templateService;
     }
 
+    // Will use StringTemplateResolver to resolve the template since template is not present in the classpath and thus needs to be resolved dynamically
     @GetMapping("/index")
     public String getData() {
         NoteTemplate note = new NoteTemplate();
         note.setNoteTemplateContent("<b>This note is for [(${patient.name})] and the practitioner writing this note is [(${practitioner.name})]</b> <br/>" +
-                "<b> Note: <i>[(${patient.otherPatientDetails})] and practitioner [(${practitioner.speciality})] </i> </b>");
+                "<b> Note: <i>[(${patient.otherPatientDetails})] and practitioner \"[(${practitioner.speciality})]\" </i> </b>");
+        note.setNoteTemplateContent("<head><title>[(${patient.name})]</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><link rel=\"shortcut icon\" href=\"[(${baseUrl})]/favicon.ico\" />\n</head>\n<body>\n<p>[(${practitioner.name})] [(${patient.name})]</p><p>[(${patient.otherPatientDetails})]</p><p><a href=\"[(${baseUrl})]/activate?key=[(${patient.name})]\"> [(${url})] </a></p><p><span>[(${practitioner.speciality})], </span><br/><em>[(${practitioner.name})].</em></p></body></html>");
 
         Practitioner practitioner = new Practitioner();
         practitioner.setName("Dr. Dread");
@@ -43,6 +52,17 @@ public class MyController {
         String processedTemplate = templateEngine.process(note.getNoteTemplateContent(), context);
 
         log.info(processedTemplate);
+        return processedTemplate;
+    }
+
+    // Will use ClassLoaderTemplateResolver to resolve the template in this case, since help.html is present in the classpath
+    @GetMapping("/try")
+    public String displayStaticContent(){
+
+        Context context = new Context();
+        context.setVariable("message", "Fame");
+
+        String processedTemplate = templateEngine.process("help", context);
         return processedTemplate;
     }
 }
--- src/main/java/com/learn/config/ThymeleafConfig.java
+++ src/main/java/com/learn/config/ThymeleafConfig.java
@@ -7,26 +7,40 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.dialect.SpringStandardDialect;
 import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.StringTemplateResolver;
-
 
 @Configuration
 @EnableWebMvc
-public class ThymeleafConfig implements WebMvcConfigurer {
-    @Bean(name ="templateResolver")
-    public StringTemplateResolver getTemplateResolver() {
-        StringTemplateResolver templateResolver = new StringTemplateResolver();
-        templateResolver.setTemplateMode(TemplateMode.TEXT);
-        return templateResolver;
-    }
+public class ThymeleafConfig implements WebMvcConfigurer{
 
-    @Bean(name ="templateEngine")
+    @Bean(name = "templateEngine")
     public SpringTemplateEngine getTemplateEngine() {
         SpringStandardDialect dialect = new SpringStandardDialect();
         dialect.setEnableSpringELCompiler(true);
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(getTemplateResolver());
+        templateEngine.addTemplateResolver(getClassLoaderTemplateResolver());
+        templateEngine.addTemplateResolver(getStringTemplateResolver());
         templateEngine.setDialect(dialect);
         return templateEngine;
     }
+
+    public ClassLoaderTemplateResolver getClassLoaderTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setOrder(1);
+        templateResolver.setCheckExistence(true);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+
+    @Bean(name = "templateResolver")
+    public StringTemplateResolver getStringTemplateResolver() {
+        StringTemplateResolver templateResolver = new StringTemplateResolver();
+        templateResolver.setTemplateMode(TemplateMode.TEXT);
+        templateResolver.setOrder(2);
+        return templateResolver;
+    }
+
 }
